7e5cde7b90e3b430fb88c40722e41b65c0d04e0a
lazalajochy <lazala1401jochy@outlook.com>
2022-12-09 19:10:52 -0400

    queue

--- dataStructure/queue/queue.js
+++ dataStructure/queue/queue.js
@@ -1,26 +1,32 @@
 class Queue{
     constructor(){
-        this.items = []
+        this.items = {}
+        this.rear = 0;
+        this.front = 0;
     };
 
     enqueue = (element) => {
-        return this.items.push(element)
+       this.items[this.rear] = element;
+       this.rear++;
     }
 
     dequeue = () => {
-        return this.items.shift()
+        const item = this.items[this.front];
+        delete  this.items[this.front];
+        this.front++
+        return item;
     }
 
     peek = () => {
-        return this.items[this.items.length -1]
+        return this.items[this.front]
     }
 
     isEmpty = () => {
-        return this.items.length === 0;
+        return this.rear - this.front === 0;
     }
 
     size = () => {
-        return this.items.length;
+        return this.rear - this.front
     }
 
     print = () => {
