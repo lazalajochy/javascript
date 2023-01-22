b419d8a1d338243de19e32f7ecb65ef11fbe8e9c
lazalajochy <lazala1401jochy@outlook.com>
2023-01-21 20:57:37 -0400

    task queue

--- event-loop/basic/timeout.js
+++ event-loop/basic/timeout.js
@@ -4,6 +4,12 @@ debugger
 
 console.log("Hello");
 
-setTimeout(() => console.log("Time"),4000);
+//setTimeout(() => console.log("Time"),4000);
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')  
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
 
 console.log("World")
