91858f9f29cc1265d97de9a83ab2b6b86aeae1bb
lazalajochy <lazala1401jochy@outlook.com>
2023-01-30 21:43:47 -0400

    linkedlist

--- dataStructure/linklist/linkListed.js
+++ dataStructure/linklist/linkListed.js
@@ -1,26 +1,66 @@
-function List(){
-    this.head = null;
-}
+/*
+Insertar un nodo en el inicio
+Insertar un nodo en una posición específica
+Insertar un nodo al final
+Eliminar un nodo en una posición específica
+Recorrer la lista para imprimir sus elementos
+Buscar un elemento en la lista
+Contar el número de nodos en la lista
+Revertir la lista.
+ */
 
-function Node(value){
+function ListOne() {
+    this.head = null;
+  }
+  
+  function NodeOne(value) {
     this.value = value;
     this.next = null;
-}
-
-List.prototype.add = function(value){
-    if(this.head === null) this.head = new Node(value)
-    else{
-        let refe = this.head;
-        while(refe.next !== null){
-            refe = refe.next
-        }
-        refe.next = new Node(value)
+  }
+  
+  ListOne.prototype.add = function (value) {
+    if (this.head === null) this.head = new NodeOne(value);
+    else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = new NodeOne(value);
     }
-}
-
-let list = new List();
-
-list.add(1);
-list.add(2);
-list.add(3);
-list.add(4);
+  };
+  
+  //Recorrer la lista para imprimir sus elementos
+  ListOne.prototype.print = function () {
+    let current = this.head;
+    let result = [];
+    while (current) {
+      result.push(current.value);
+      current = current.next;
+    }
+    return result;
+  };
+  
+  //Buscar un elemento en la lista
+  ListOne.prototype.search = function (value) {
+    let current = this.head;
+    let result;
+    console.log(value)
+    while (current) {
+      result = current.value;
+      current = current.next;
+      if(result === value)return true
+      
+    }
+  return false
+  
+  };
+  
+  const listone = new ListOne();
+  listone.add(2);
+  listone.add(4);
+  listone.add(25);
+  
+  console.log(listone);
+  
+  console.log(listone.search(2));
+  
