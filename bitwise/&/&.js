579d4494265b197d80242ad55a487002530ccbf1
lazalajochy <lazala1401jochy@outlook.com>
2023-02-22 00:35:08 -0400

    &

--- bitwise/&/&.js
+++ bitwise/&/&.js
@@ -5,7 +5,7 @@ bitwise(a, b);
 function bitwise(a,b){
     console.log(a.toString(2), " ", b.toString(2));
     /*
-    111 & 110 ==> 110, en base a decimal el resultado es 6
+    111 & 110 ==> en base a binario el resultado es 110, en base a decimal el resultado es 6
     */
     console.log(a & b)
 }
