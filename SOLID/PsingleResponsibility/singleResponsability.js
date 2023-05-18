2f7e286842262a75a173b43aca557fc7a3d73f25
lazalajochy <lazala1401jochy@outlook.com>
2023-05-18 17:29:31 -0400

    fixing the misunderstood

--- SOLID/PsingleResponsibility/singleResponsability.js
+++ SOLID/PsingleResponsibility/singleResponsability.js
@@ -1,20 +1,28 @@
-class SingleREsponsability{
-    constructor(brand, model){
-        this.brand = brand;
-        this.model = model;
+class Sumador {
+    sumar(numeros) {
+      let resultado = 0;
+      for (let numero of numeros) {
+        resultado += numero;
+      }
+      return resultado;
     }
+  }
+  
+  // Ejemplo de uso
+  const sumador = new Sumador();
+  const numeros = [1, 2, 3, 4, 5];
+  const resultado = sumador.sumar(numeros);
+  console.log(`La suma de los números es: ${resultado}`);
 
-    getBrand = () => {
-        return this.brand;
-    };
 
-    getModel = () => {
-        return this.model;
-    };
-}
 
-const singleResponsability = new SingleREsponsability("toyota","mbs155");
+  /*
+En este ejemplo, la clase Sumador tiene la única responsabilidad de realizar sumas de números. La función sumar toma un arreglo de números y devuelve la 
+suma de ellos. No está involucrada en la obtención de los números, ni en la presentación del resultado.
 
-console.log(singleResponsability.getBrand());
-
-console.log(singleResponsability.getModel());
+Al aplicar el SRP, separamos la responsabilidad de realizar la suma en una clase dedicada a esa función específica. Esto permite un código más modular y
+fácil de mantener, ya que si en el futuro necesitamos realizar cambios en la lógica de suma (por ejemplo, considerar solo los números pares o implementar 
+una suma con condiciones adicionales), solo tendríamos que modificar la clase Sumador, sin afectar otras partes del sistema que no están directamente 
+relacionadas con la suma de números.
+  
+  */
